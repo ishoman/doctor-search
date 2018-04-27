ef34e552af1b2a7151e8a61be0f3443805f7795e
Isaac Shoman <isaac.shoman@gmail.com>
2018-04-27 12:21:12 -0700

    Add for loop to output function

--- src/doctor.js
+++ src/doctor.js
@@ -1,12 +1,11 @@
 class API {
 
-makeCall (input) {
-  debugger;
+makeCall (input, displayResult, displayError, displayNoResults) {
   let promise = new Promise(function(resolve, reject) {
     let request = new XMLHttpRequest();
     let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${input}&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
     request.onload = function() {
-      if (this.status === 300) {
+      if (this.status === 200) {
         resolve(request.response);
       } else {
         reject(Error(request.statusText));
@@ -18,9 +17,13 @@ makeCall (input) {
 
     promise.then(function(response) {
       const body = JSON.parse(response);
-      console.log(body);
+      if (body.data.length === 0){
+        displayNoResults(body);
+      } else {
+        displayResult(body);
+      }
     }), function (error) {
-      $("#output").append(`There was an error processing your request: ${error.message}`);
+      displayError(error)
     }
   }
 
--- src/main.js
+++ src/main.js
@@ -4,12 +4,32 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+let displayResult = function (body) {
+  let doctorArray = body.data
+  console.log(doctorArray);
+  for (let i = 0; i < doctorArray.length; i++) {
+    $("#output").append(`<li>${body.data[i].profile.first_name} ${body.data[i].profile.last_name}</li>`);
+  }
+}
+
+let displayError = function(error) {
+  $("#errorOutput").text(`There was an error processing your request: ${error.message}`);
+}
+
+let displayNoResults = function(body) {
+  $("#errorOutput").text(`Your search returned no results.`);
+}
+
 $(document).ready(function() {
   $("#doctor").submit(function(event) {
     event.preventDefault();
-    debugger;
+    $('#output').resetField();
     let input = $('#search').val();
     let api = new API
-    api.makeCall(input);
+    api.makeCall(input, displayResult, displayError, displayNoResults);
   });
+  let resetField = function(){
+    $('#output').val("")
+    $()
+  }
 });
