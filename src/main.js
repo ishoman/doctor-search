aa290e7f76308e9ea019e6b15c15a0425fc4b3e9
Isaac Shoman <isaac.shoman@gmail.com>
2018-04-27 16:56:46 -0700

    Add completed functionality to appending list

--- src/main.js
+++ src/main.js
@@ -4,25 +4,37 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 let displayResult = function (body) {
   $('#output').empty();
   $('#errorOutput').empty();
-  // let yesNo = function(){
-  //   if (body.data[i].practices[0].accepts_new_patients === true) {
-  //     return "Yes";
-  //   } else {
-  //     return "No";
-  //   }
-  // }
   let doctorArray = body.data
   for (let i = 0; i < doctorArray.length; i++) {
-    $("#output").append(`<ul id="doctorNames">${body.data[i].profile.first_name} ${body.data[i].profile.last_name}
+    let acceptingPatients = function(){
+      if (body.data[i].practices[0].accepts_new_patients === true) {
+        return "Yes";
+      } else {
+        return "No";
+      }
+    };
+    let patientsYesNo = acceptingPatients();
+
+    let websiteAvailable = function(){
+      if (body.data[i].practices[0].website !== undefined) {
+        return body.data[i].practices[0].website;
+      } else {
+        return "N/A";
+      }
+    }
+
+    let websiteYesNo = websiteAvailable();
+    $("#output").append(`<ul id="doctorNames">${body.data[i].profile.first_name} ${body.data[i].profile.last_name}, ${body.data[i].profile.title}
                           <div id="doctorInfo">
                             <ul>
-                              <li>Accepting new patients: ${body.data[i].practices[0].accepts_new_patients}</li>
-                              <li>address</li>
-                              <li>phone number</li>
-                              <li>website</li>
+                              <li>Accepting new patients: ${ patientsYesNo }</li>
+                              <li>Address: ${body.data[i].practices[0].visit_address.street}, ${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state} ${body.data[i].practices[0].visit_address.zip}</li>
+                              <li>Phone Number: ${body.data[i].practices[0].phones[0].number} </li>
+                              <li>Website: <a href="${websiteYesNo}">${websiteYesNo}</a></li>
                               <li>${body.data[i].profile.bio}</li>
                             <ul>
                           </div>
@@ -51,8 +63,8 @@ $(document).ready(function() {
     api.makeCall(input, displayResult, displayError, displayNoResults);
   });
 
-$("li#doctorNames").click(function(){
-  $("div#doctorInfo").toggle();
-});
+// $("li#doctorNames").click(function(){
+//   $("div#doctorInfo").toggle();
+// });
 
 });
