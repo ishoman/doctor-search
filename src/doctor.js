a829bb5a814775e40238d7e43fbd2d47233015eb
Isaac Shoman <isaac.shoman@gmail.com>
2018-04-27 09:57:07 -0700

    Add successful API call on page load

--- src/doctor.js
+++ src/doctor.js
@@ -1,17 +1,23 @@
 class API {
 
-  makeCall (displayResult, displayError){
-    let promise = new Promise(function (resolve, request){
-      let request = XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=chest%20pain&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject (request.statusText);
-        }
+  makeCall () {
+  let promise = new Promise(function(resolve, reject) {
+    let request = new XMLHttpRequest();
+    let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+    request.onload = function() {
+      if (this.status === 200) {
+        resolve(request.response);
+      } else {
+        reject(Error(request.statusText));
       }
-    })
+    }
+    request.open("GET", url, true);
+    request.send();
+  });
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      console.log(body, "hello");
+    });
   }
 
 }
--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-import { } from './doctor.js'; //ANY FUNCTIONS YOU ARE IMPORTING
+import { API } from './doctor.js';
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+let api = new API
+api.makeCall();
 $(document).ready(function() {
   $("#doctor").submit(function(event) {
     event.preventDefault();
-
+    console.log("hello");
   });
 });
